1b1f3df33cae5c4168eea16ed2660fc1b7e67c63
Christopher Conley <pittpir@gmail.com>
2018-12-03 21:12:08 -0600

    Created Game Logic Update

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -8,23 +8,43 @@ var cardFour = "king";
 
 
 var cards = [ "queen", "queen", "king", "king" ];
-
 var cardsInPlay = [];
-var cardOne = cards[1];
-cardsInPlay.push(cardOne);
-
-cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-
-if (cardsInPlay.length === 2)
-{
-    console.log("The length is equal to 2");
-} 
-if (cardsInPlay[0] === cardsInPlay[1] )
-{
-    alert("You found a match");
-} else {
-    alert("Sorry, try again!");
+function checkForMatch() {
+    if (cardsInPlay[0] === cardsInPlay[1]) {
+        console.log("You found a match!");
+        } else {
+        console.log("Sorry, try again.");
+        }
 }
 
-console.log("User flipped " + cardsInPlay[1]);
+function flipCard (CardId) {
+
+    /*
+    var cardOne = cards[1];
+    cardsInPlay.push(cardOne);
+    
+    cardTwo = cards[2];
+    cardsInPlay.push(cardTwo);
+*/
+    console.log("User flipped " + cards[CardId]);
+    cardsInPlay.push(cards[CardId]);
+    checkForMatch();
+    /*
+    if (cardsInPlay.length === 2)
+    {
+        console.log("The length is equal to 2");
+    } 
+    if (cardsInPlay[0] === cardsInPlay[1] )
+    {
+        //alert("You found a match");
+    } else {
+        //alert("Sorry, try again!");
+    }
+*/
+    //console.log("User flipped " + cardsInPlay[1]);
+
+}
+
+flipCard(0);
+flipCard(2);
+
