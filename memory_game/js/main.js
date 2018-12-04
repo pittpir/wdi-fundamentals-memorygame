a134495b5868625e1d4172b25ddade09687fb5b0
Christopher Conley <pittpir@gmail.com>
2018-12-03 20:59:58 -0600

    Created Game Logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,30 @@
 console.log("Up and running!");
+/*
 var cardOne = "queen";
 var CardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
+*/
 
-console.log("User flipped " + cardOne);
+
+var cards = [ "queen", "queen", "king", "king" ];
+
+var cardsInPlay = [];
+var cardOne = cards[1];
+cardsInPlay.push(cardOne);
+
+cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
+
+if (cardsInPlay.length === 2)
+{
+    console.log("The length is equal to 2");
+} 
+if (cardsInPlay[0] === cardsInPlay[1] )
+{
+    alert("You found a match");
+} else {
+    alert("Sorry, try again!");
+}
+
+console.log("User flipped " + cardsInPlay[1]);
